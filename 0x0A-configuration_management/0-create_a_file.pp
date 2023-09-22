182b3e8e0667b0eb4048259a6c6658fef9bf7bb0
nyashakagudah <kagudagrace@gmail.com>
2023-09-22 12:37:10 -0700

    0x0A

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Manifest that manages school configuration file
+file { 'school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
